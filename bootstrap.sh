15df1434b46c6be732d7e78fb26bfd3ab0156116
Tommy Back <tommy.back@iki.fi>
2015-04-29 13:44:37 +0200

    Updates bootstrap so one can provide a path for cleaner symlinking.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+dir="dotfiles"
+
+if [[ "$1" != "" ]] ; then
+    dir=$1
+fi
+
 # set link for platform specific files
 rm -f platform
 if [[ `uname` -eq "Darwin" ]] ; then
@@ -11,27 +17,26 @@ fi
 cd $HOME
 
 files="\
-aliases\
-bash_profile\
-bash_prompt\
-bashrc\
-bash_logout\
-emacs\
-emacs.d\
-exports\
-gitconfig\
-hushlogin\
-inputrc\
-screenrc\
-wgetrc"
+  aliases\
+  bash_profile\
+  bash_prompt\
+  bashrc\
+  bash_logout\
+  emacs\
+  emacs.d\
+  exports\
+  gitconfig\
+  hushlogin\
+  inputrc\
+  screenrc\
+  wgetrc"
 
 for l in $files ; do
-  if [[ -h .$l ]] ; then
-    rm .$l
-  elif [[ -f .$l || -d .$l ]] ; then
-    mkdir -p tmp
-    mv .$l tmp/
-  fi
-  ln -s dotfiles/.$l .$l
+    if [[ -h .$l ]] ; then
+	rm .$l
+    elif [[ -f .$l || -d .$l ]] ; then
+	mkdir -p tmp
+	mv .$l tmp/
+    fi
+    ln -s $dir/.$l .$l
 done
-
